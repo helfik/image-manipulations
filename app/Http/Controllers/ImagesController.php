5c908b168e8d3eb9b185094d6b0b789efbc0d8f2
Nigar Kazim <niko.kazim@gmail.com>
2019-06-27 22:54:29 +0400

    optimization

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -16,14 +16,18 @@ class ImagesController extends Controller
     public function processImage() {
         Image::configure(array('driver' => 'gd'));
 
-        $image = Image::make(Input::file('image')->getRealPath())->blur(15)->colorize(-10, 0, 30)->text('Superhero Cheesecake', 220, 220, function($font) {
-            $font->file('fonts/JosefinSans-Bold.ttf');
-            $font->size(50);
-            $font->color(array(139, 0, 0, 0.5));
-            $font->align('center');
-            $font->valign('center');
-            $font->angle(45);
-        });;
+        $image = Image::make(Input::file('image')->getRealPath())
+            ->blur(15)
+            ->colorize(-10, 0, 30)
+            ->brightness(15)
+            ->text('Superhero Cheesecake', 220, 220, function($font) {
+                $font->file('fonts/JosefinSans-Bold.ttf');
+                $font->size(50);
+                $font->color(array(139, 0, 0, 0.5));
+                $font->align('center');
+                $font->valign('center');
+                $font->angle(45);
+            });
 
         $image->save();
 
