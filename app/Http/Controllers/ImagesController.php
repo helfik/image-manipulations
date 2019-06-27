34b3a96a4af125f76ae9266a533a53c5bb785b50
Nigar Kazim <niko.kazim@gmail.com>
2019-06-27 22:46:41 +0400

    font color

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -16,13 +16,13 @@ class ImagesController extends Controller
     public function processImage() {
         Image::configure(array('driver' => 'gd'));
 
-        $image = Image::make(Input::file('image')->getRealPath())->pixelate(12)->text('Superhero Cheesecake', 0, 0, function($font) {
-            $font->file('JosefinSans-Bold.ttf');
+        $image = Image::make(Input::file('image')->getRealPath())->blur(15)->colorize(-10, 0, 30)->text('Superhero Cheesecake', 220, 220, function($font) {
+            $font->file('fonts/JosefinSans-Bold.ttf');
             $font->size(50);
-            $font->color('#ff0000');
+            $font->color(array(139, 0, 0, 0.5));
             $font->align('center');
             $font->valign('center');
-            $font->angle(90);
+            $font->angle(45);
         });;
 
         $image->save();
